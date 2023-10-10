032ee0c608a7f7e85a130d3a7b58231d2dd4c823
code-with-ayesha <ayeshaijaz298@gmail.com>
2023-10-10 13:46:39 +0500

    update

--- function.js
+++ function.js
@@ -1,19 +1,12 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.division = exports.multiplication = exports.subtraction = exports.sum = void 0;
-function sum(a, b) {
+export function sum(a, b) {
     return a + b;
 }
-exports.sum = sum;
-function subtraction(a, b) {
+export function subtraction(a, b) {
     return a - b;
 }
-exports.subtraction = subtraction;
-function multiplication(a, b) {
+export function multiplication(a, b) {
     return a * b;
 }
-exports.multiplication = multiplication;
-function division(a, b) {
+export function division(a, b) {
     return a / b;
 }
-exports.division = division;
